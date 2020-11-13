b82459456d9f90f857788b7e9e130b21900eaf3b
juliabenedicto <74271084+juliabenedicto@users.noreply.github.com>
2020-11-13 15:30:18 +0100

    13.11 15h30

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-
-
-$(function() {
-    $(".toggle").on("click", function() {
-        if ($(".item").hasClass("active")) {
-            $(".item").removeClass("active");
-            $(this).find("a").html("<i class='fas fa-bars'></i>");
-        } else {
-            $(".item").addClass("active");
-            $(this).find("a").html("<i class='fas fa-times'></i>");
-        }
-    });
-});
+/* Para quitar y añadir la class "responsive" al topnav cuando el usuario hace click en el botón */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
