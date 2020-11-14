6aceac8feac5548dd3d106dd88771b4b4aa4fa6d
juliabenedicto <74271084+juliabenedicto@users.noreply.github.com>
2020-11-14 19:57:56 +0100

    test PHP

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php include_once 'templates/header.php'; ?>
+
+<main>
+    <div class="hero ">
+        <div class="contenedor-hero ">
+            <h1>Haki App</h1>
+            <p>¿Has sufrido una situación de ciberacoso?</p>
+            <p>Haki App te ayuda a denunciarlo de forma <b>fácil</b> y <b>segura</b>.</p>
+            <div class="descarga ">
+                <a href="# " class="boton-color2 ">Descarga Haki App</a>
+                <img src="/img/3 PRMERES PANTALLES SENSE FONS_opt.png " alt="screens-hakiapp " class="img-centrada ">
+            </div>
+        </div>
+    </div>
+
+    <div class="como-funciona contenedor ">
+        <h2>¿Cómo funciona?</h2>
+        <div class="contenedor-servicios ">
+            <div class="servicio ">
+                <h3>Descarga Haki App</h3>
+                <div class="iconos ">
+                    <i class="far fa-arrow-alt-circle-down "></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut id ullam sit tempore nisi laudantium vel explicabo, consequatur optio debitis porro est architecto ut et delectus? Perferendis praesentium reprehenderit suscipit?</p>
+            </div>
+            <div class="servicio ">
+                <h3>Captura el ciberacoso</h3>
+                <div class="iconos ">
+                    <i class="fas fa-mobile-alt "></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut id ullam sit tempore nisi laudantium vel explicabo, consequatur optio debitis porro est architecto ut et delectus? Perferendis praesentium reprehenderit suscipit?</p>
+            </div>
+            <div class="servicio ">
+                <h3>Genera la denuncia</h3>
+                <div class="iconos ">
+                    <i class="far fa-file-alt "></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut id ullam sit tempore nisi laudantium vel explicabo, consequatur optio debitis porro est architecto ut et delectus? Perferendis praesentium reprehenderit suscipit?</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<footer class="site-footer ">
+
+    <div class="logo-footer ">
+        <a href="/index.html "><img src="/img/logo-haki-blanco.png " alt="logo-hakiapp "></a>
+        <p>Todos los derechos reservados - 2020</p>
+    </div>
+
+
+</footer>
+
+</body>
+<script>
+</script>
+
+</html>
